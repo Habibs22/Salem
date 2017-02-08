c1be982b151cc069fc7ab61000a8b6938c4f1597
Habibs22 <king.salem1022@yahoo.com>
2017-02-08 15:43:52 +0800

    Update anyco.php

--- anyco.php
+++ anyco.php
@@ -1,19 +1,23 @@
 <?php
 require('anyco_ui.inc');
-$conn=oci_connect('hr','hr','//localhost/xe');
-ui_print_header('Depatments');
-do_query($comm,'SELECT * from Departments');
-wi_print_footer(date(Y-m-d H:i:s));
-function do_query($comm,$query);
-$stid=oci_parse($comm,$query);
-$r*oci_execute($stid,OCI_DEFAULT);
-}
-print'<table border="1"<';
-while($row=oci_fetch_array($stid,OCI_RETURN_NULLS)){
-print'<tr>';
-foreach($row as $item){
-print '<td>'.
-($item ? htmlentities($item):"&nbsp;.'</td>';}
-print '</tr>;}
-print '</table>';}
 
+$conn=oci_connect('hr','hr','//localhost/xe');
+
+ui_print_header('Departments');
+do_query($conn,'SELECT * from Departments');
+ui_print_footer(date('Y-m-d H:i:s'));
+
+function do_query($conn,$query){
+	$stid=oci_parse($conn,$query);
+	$r=oci_execute($stid,OCI_DEFAULT);
+
+	print '<table border="1">';
+
+	while($row=oci_fetch_array($stid,OCI_RETURN_NULLS)){
+		print '<tr>';
+		foreach($row as $item){
+			print '<td>'.
+				($item ? htmlentities($item):'&nbsp;').'</td>';}
+		print '</tr>' ;}
+	print '</table>' ;}
+?>
